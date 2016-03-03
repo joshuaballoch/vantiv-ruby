5b85bedab213155d17e4b8ebef605719868a9e06
Joshua Balloch <joshuaballoch@gmail.com>
2016-03-03 14:38:18 -0500

    Refactor Auth Response to use inherited methods from Api::Response:
    * SaleResponse and AuthResponse are nearly identical, but not going to
      abstract them or deal with inheritance yet.

--- lib/vantiv/api/authorization_response.rb
+++ lib/vantiv/api/authorization_response.rb
@@ -9,46 +9,31 @@ module Vantiv
       }
 
       def success?
-        !failure?
+        !api_level_failure? && transaction_approved?
       end
 
       def failure?
-        api_level_failure? || authorization_unsuccessful?
-      end
-
-      def auth_response_code
-        auth_response["response"]
-      end
-
-      def transaction_id
-        auth_response["TransactionID"]
-      end
-
-      def message
-        auth_response["message"]
+        !success?
       end
 
       def insufficient_funds?
-        auth_response_code == ResponseCodes[:insufficient_funds]
+        response_code == ResponseCodes[:insufficient_funds]
       end
 
       def invalid_account_number?
-        auth_response_code == ResponseCodes[:invalid_account_number]
+        response_code == ResponseCodes[:invalid_account_number]
       end
 
       private
 
-      def auth_response
-        body["litleOnlineResponse"]["authorizationResponse"]
+      def transaction_approved?
+        response_code == ResponseCodes[:approved]
       end
 
-      def authorization_successful?
-        auth_response_code == ResponseCodes[:approved]
+      def transaction_response_name
+        "authorizationResponse"
       end
 
-      def authorization_unsuccessful?
-        !authorization_successful?
-      end
     end
   end
 end

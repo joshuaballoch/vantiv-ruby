9984608b2734e89791dc9a2f4c0b493e1a17260f
Scott Bartell <me@scottbartell.com>
2016-05-13 12:43:47 -0400

    refactor and move duplicated logic into subject

--- spec/authorization_spec.rb
+++ spec/authorization_spec.rb
@@ -4,7 +4,7 @@ describe "auth" do
   let(:customer_external_id) { "1234" }
   let(:payment_account_id) { test_account.payment_account_id }
 
-  subject(:run_auth) do
+  subject(:response) do
     Vantiv.auth(
       amount: 10000,
       payment_account_id: payment_account_id,
@@ -19,18 +19,15 @@ describe "auth" do
     let(:test_account) { Vantiv::TestAccount.valid_account }
 
     it "returns success response" do
-      response = run_auth
       expect(response.success?).to eq true
     end
 
     it "returns a transaction ID" do
-      response = run_auth
       expect(response.transaction_id).not_to eq nil
       expect(response.transaction_id).not_to eq ""
     end
 
     it "returns a '000' response code" do
-      response = run_auth
       expect(response.response_code).to eq('000')
     end
   end
@@ -39,29 +36,24 @@ describe "auth" do
     let(:test_account) { Vantiv::TestAccount.insufficient_funds }
 
     it "returns a failure response" do
-      response = run_auth
       expect(response.success?).to eq false
       expect(response.failure?).to eq true
     end
 
     it "returns a transaction ID" do
-      response = run_auth
       expect(response.transaction_id).not_to eq nil
       expect(response.transaction_id).not_to eq ""
     end
 
     it "gives a human readable reason" do
-      response = run_auth
       expect(response.message).to match(/insufficient funds/i)
     end
 
     it "notifies that it is insufficient funds" do
-      response = run_auth
       expect(response.insufficient_funds?).to eq true
     end
 
     it "returns a '110' response code" do
-      response = run_auth
       expect(response.response_code).to eq('110')
     end
   end
@@ -70,29 +62,24 @@ describe "auth" do
     let(:test_account) { Vantiv::TestAccount.invalid_account_number }
 
     it "returns a failure response" do
-      response = run_auth
       expect(response.success?).to eq false
       expect(response.failure?).to eq true
     end
 
     it "returns a transaction ID" do
-      response = run_auth
       expect(response.transaction_id).not_to eq nil
       expect(response.transaction_id).not_to eq ""
     end
 
     it "gives a human readable reason" do
-      response = run_auth
       expect(response.message).to match(/invalid account number/i)
     end
 
     it "responds to invalid_account_number?" do
-      response = run_auth
       expect(response.invalid_account_number?).to eq true
     end
 
     it "returns a '301' response code" do
-      response = run_auth
       expect(response.response_code).to eq('301')
     end
   end
@@ -101,25 +88,21 @@ describe "auth" do
     let(:test_account) { Vantiv::TestAccount.pick_up_card }
 
     it "returns a failure response" do
-      response = run_auth
       expect(response.success?).to eq false
       expect(response.failure?).to eq true
     end
 
     it "returns a transaction ID" do
-      response = run_auth
       expect(response.transaction_id).not_to eq nil
       expect(response.transaction_id).not_to eq ""
     end
 
     it "gives a human readable reason" do
-      response = run_auth
       expect(response.message).not_to eq nil
       expect(response.message).not_to eq ""
     end
 
     it "returns a '303' response code" do
-      response = run_auth
       expect(response.response_code).to eq('303')
     end
   end
@@ -137,7 +120,6 @@ describe "auth" do
     end
 
     it "responds that the authorization failed" do
-      response = run_auth
       expect(response.failure?).to eq true
       expect(response.api_level_failure?).to eq true
     end
